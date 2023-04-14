f38f41f24d76e3d00e0a278baeb411caec6a9e85
Mike Wazovzky <mike.wazovzky@gmail.com>
2023-04-14 21:58:22 +0400

    RBAC. Associate User with Client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Client extends Model
 {
@@ -17,5 +18,10 @@ class Client extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->BelongsToMany(User::class);
     }
 }
--- app/Nova/Client.php
+++ app/Nova/Client.php
@@ -2,8 +2,8 @@
 
 namespace App\Nova;
 
-use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -56,6 +56,8 @@ class Client extends Resource
                 ->rules('required', 'max:255'),
 
             BelongsTo::make('Company'),
+
+            BelongsToMany::make('Users'),
         ];
     }
 
