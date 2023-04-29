83b742c1f9833751a6ecc93b3a5a692d84918b4c
Mike Wazovzky <mike.wazovzky@gmail.com>
2023-04-29 17:39:33 +0400

    RBAC. Check if User has Permission

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,7 +2,7 @@
 
 namespace App\Policies;
 
-use App\Models\Client;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -19,7 +19,7 @@ class UserPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Client $client): bool
+    public function view(User $user, User $model): bool
     {
         return false;
     }
@@ -35,7 +35,7 @@ class UserPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Client $client): bool
+    public function update(User $user, User $model): bool
     {
         return false;
     }
@@ -43,7 +43,7 @@ class UserPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Client $client): bool
+    public function delete(User $user, User $model): bool
     {
         return false;
     }
@@ -51,7 +51,7 @@ class UserPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Client $client): bool
+    public function restore(User $user, User $model): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class UserPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Client $client): bool
+    public function forceDelete(User $user, User $model): bool
     {
         return false;
     }
